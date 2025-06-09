22527901e9d758d4351e01865ad6e2dc0bdc7565
Marta <m.olejniczak550@gmail.com>
2025-06-09 19:06:41 +0200

    displaying text on website

--- zadanie1.js
+++ zadanie1.js
@@ -1,1 +1,4 @@
-console.log('hello');
+var paragraph = document.createElement('p');
+paragraph.textContent = 'Hello';
+document.body.appendChild(paragraph);
+//# sourceMappingURL=zadanie1.js.map
--- zadanie1.ts
+++ zadanie1.ts
@@ -1,1 +1,8 @@
-console.log('hello'); 
+// Tworzymy nowy element <p>
+const paragraph: HTMLParagraphElement = document.createElement('p');
+
+// Ustawiamy zawartość tekstową
+paragraph.textContent = 'Hello';
+
+// Dodajemy do body dokumentu
+document.body.appendChild(paragraph);
